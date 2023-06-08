161c2390f894f21d500b0ac64bd0dd470224eaa4
Akazaa <93519486+AkazaaMoon@users.noreply.github.com>
2023-06-08 14:48:19 +0700

    [skip ci]

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Kernel Sources!
-git clone --depth=1 https://github.com/c3eru/kernel_xiaomi_chime -b tithonia $CIRRUS_WORKING_DIR/KERNEL
+git clone --depth=1 https://github.com/c3eru/kernels -b main $CIRRUS_WORKING_DIR/KERNEL
 
 # Tool Chain
 # Proton Clang ---
