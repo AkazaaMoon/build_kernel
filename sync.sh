addb54afd7fb2fee221b5fb61fa6a4ecf96dfb67
Akazaa <93519486+AkazaaMoon@users.noreply.github.com>
2023-06-10 12:02:22 +0700

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Kernel Sources!
-git clone --depth=1 https://github.com/c3eru/kernel_citrus -b a $CIRRUS_WORKING_DIR/KERNEL
+git clone --depth=1 https://github.com/c3eru/kernel_citrus -b 13 $CIRRUS_WORKING_DIR/KERNEL
 # git clone --depth=1 https://github.com/c3eru/KernelSU -b main $CIRRUS_WORKING_DIR/KERNEL
 
 # Tool Chain
