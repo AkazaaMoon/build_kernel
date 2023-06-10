3cd51262d5d15af21e0f26c0b023e48bddd53dc1
Akazaa <93519486+AkazaaMoon@users.noreply.github.com>
2023-06-10 17:22:01 +0700

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -6,6 +6,6 @@ git clone --depth=1 https://github.com/c3eru/kernel_citrus -b 13 $CIRRUS_WORKING
 
 # Tool Chain
 # Proton Clang ---
- git clone --depth=1 https://github.com/kdrag0n/proton-clang $CIRRUS_WORKING_DIR/PROTON-CLANG
+ git clone --depth=1 https://github.com/greenforce-project/clang-llvm $CIRRUS_WORKING_DIR/PROTON-CLANG
  git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 $CIRRUS_WORKING_DIR/GCC
  git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 $CIRRUS_WORKING_DIR/GCC32
